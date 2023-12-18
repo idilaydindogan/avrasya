8b6675f900647fe751484374f93521a984a862ac
idilaydin <idilaydindogan@gmail.com>
2023-12-18 13:29:33 +0300

    navbar burger menu added, logo improved

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import React from "react";
+import logo from "../../public/logo.png";
 
 function Footer() {
 	return (
 		<div className="h-36 w-full bg-footerBody border-t border-t-secondary_Navy">
-			Footer
+			<Image src={logo} width={180} height={180} />
 		</div>
 	);
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+// import logo from "../../public/company_logo.png";
 import logo from "../../public/logo.png";
 import Link from "next/link";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
-	const navbar_titles = [
-		{ name: "Sektörler", link: "/" },
-		{ name: "Teknolojiler", link: "/" },
-	];
+	const [visiable, setVisible] = useState(false);
 	return (
 		<nav className="w-full h-48">
 			<div className="md:flex justify-evenly items-center text-white hidden ">
@@ -18,10 +19,39 @@ const Navbar = () => {
 					<Image src={logo} width={400} height={400} />
 				</Link>
 				<Link href={"/"}>Teknolojiler</Link>
-				<Link href={"/"}>Iletişim</Link>
+				<Link href={"/"}>İletişim</Link>
 			</div>
-			<div className="flex justify-evenly items-center text-white">
-				<Link href={"/"}>Home</Link>
+			<div className="flex justify-between text-white md:hidden">
+				<Link href={"/"}>
+					<Image
+						src={logo}
+						width={300}
+						height={300}
+						className="-left-12 -top-4 absolute"
+					/>
+				</Link>
+				{!visiable && (
+					<button className="pt-16 pr-8" onClick={() => setVisible(true)}>
+						<GiHamburgerMenu size={22} />
+					</button>
+				)}
+				{visiable && (
+					<div className=" flex flex-col pl-10 pt-10 space-y-4 bg-white  text-primary_Navy w-[50%] h-screen ">
+						<div className="w-full flex justify-end pr-6">
+							<RxCross2
+								className="cursor-pointer"
+								size={22}
+								onClick={() => setVisible(false)}
+							/>
+						</div>
+						<div className="flex flex-col space-y-4">
+							<Link href={"/"}>Home</Link>
+							<Link href={"/"}>Sektörler</Link>
+							<Link href={"/"}>Teknolojiler</Link>
+							<Link href={"/"}>İletişim</Link>
+						</div>
+					</div>
+				)}
 			</div>
 		</nav>
 	);
